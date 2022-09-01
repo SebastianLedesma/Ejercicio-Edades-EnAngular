0b6e786b9b0f70bbfb8b26e4db95efd86c295962
Ricardo Ledesma <ricardoledesmainfolaboral@gmail.com>
2022-08-31 22:22:46 -0300

    Formulario agregado.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EjercicioEdades';
+
+  edadUno: number = 0;
+  edadDos: number = 0;
+  promedio: number = 0;
+  sumaEdades: number = 0;
+
+  calcular(): void {
+    this.sumaEdades = this.edadUno + this.edadDos;
+    this.promedio = (this.edadUno + this.edadDos) / 2;
+  }
+
+  borrar():void{
+    this.edadUno=0;
+    this.edadDos=0;
+    this.promedio=0;
+    this.sumaEdades=0;
+  }
+
 }
